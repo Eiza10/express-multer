cba106d99454d3d594c630e1b8f43d97795323fb
Haritz Eizaguirre Maneiro <133903851+Eiza10@users.noreply.github.com>
2024-12-10 18:10:18 +0000

    bukatuta2

--- routes/profile.js
+++ routes/profile.js
@@ -2,17 +2,21 @@ var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
 const path = require('path')
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.redirect('form.html');
 });
 
-const uploadDir = path.join(__dirname, '..', 'public', 'uploads');
+const uploadsDir = path.join(__dirname, '..', 'public', 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir)
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, uploadDir)
+      cb(null, uploadsDir)
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
